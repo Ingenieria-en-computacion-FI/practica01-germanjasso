9a4f6f627992a66c2ebd324653a9b3cda4082fb1
germanjasso <germanjasso610@gmail.com>
2026-02-21 14:50:37 -0600

    Update ej10.c

--- soluciones/ej10.c
+++ soluciones/ej10.c
@@ -5,3 +5,7 @@ int main(){
    
     return 0;
 }
+//Si falla:
+//explica la causa: falla porque el arreglo es mas grande de lo que puede guardar la memoria stack (pila)
+//indica dónde se almacena: en la memoria stack
+//propone solución sin memoria dinámica: guardarlo en la memoria global, basicamente poner la variable fuera del main()
